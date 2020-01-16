760a6d33f80e99a6a024dca4e9aaae6c3551b5de
diopek <diopeker@gmail.com>
2020-01-16 01:59:33 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+    // Option - 1
+    // this.handleChange = this.handleChange.bind(this);
+  }
+  // Option - 1
+  // handleChange(e) {
+  //   this.state({ searchField: e.target.value });
+  // }
+  // Option - 2
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then(response => response.json())
+  //     .then(users => this.setState({ monsters: users }));
+  // }
+
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
