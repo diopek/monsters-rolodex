839fc6ff15ade6665e5419bed6781e243072ec3f
diopek <diopeker@gmail.com>
2020-01-16 01:57:22 -0500

    Updates

---
+++ precache-manifest.11f2ef6846b6e9d4c8a614b9efa9d9aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b5751bf1c66025c1827f9657603716b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "565db0210fac80ab6489",
+    "url": "/monsters-rolodex/static/css/main.d599f085.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "565db0210fac80ab6489",
+    "url": "/monsters-rolodex/static/js/main.d3043e4f.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
